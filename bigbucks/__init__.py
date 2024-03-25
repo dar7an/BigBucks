d01416e475760bfa4c5b04021318dfd3cc580ca5
Darshan Dhande <dad86@duke.edu>
2024-03-25 17:23:23 -0400

    refractor, requirements, gitignore

--- bigbucks/__init__.py
+++ bigbucks/__init__.py
@@ -6,16 +6,16 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     
-    # Define the path to the Database folder within the Flask application directory
-    db_folder = os.path.join(app.root_path, 'Database')
+    # Define the path to the database folder within the Flask application directory
+    db_folder = os.path.join(app.root_path, 'database')
     
-    # Ensure the Database folder exists
+    # Ensure the database folder exists
     try:
         os.makedirs(db_folder)
     except OSError:
         pass
     
-    # Define the path to the database file inside the Database folder
+    # Define the path to the database file inside the database folder
     db_path = os.path.join(db_folder, 'stock_database.db')
     
     app.config.from_mapping(
