ce68e420a2e93fb54135a75dafb6c73b5b324589
Darshan Dhande <dad86@duke.edu>
2024-04-08 13:01:12 -0400

    admin functionality

--- bigbucks/__init__.py
+++ bigbucks/__init__.py
@@ -45,6 +45,10 @@ def create_app(test_config=None):
     app.register_blueprint(account.bp)
     app.add_url_rule('/account', endpoint='account')
 
+    from . import admin
+    app.register_blueprint(admin.bp)
+    app.add_url_rule('/admin', endpoint='admin')
+
     from . import db
     db.init_app(app)
 
---
+++ bigbucks/admin.py
@@ -0,0 +1,75 @@
+import requests
+from .config import API_KEY
+from flask import Blueprint, render_template, g
+from werkzeug.exceptions import abort
+from datetime import datetime  # Import the datetime module to get the current date
+from .db import get_db
+
+bp = Blueprint('admin', __name__, url_prefix='/admin')
+
+
+def check_admin():
+    if g.user is None or g.user['role'] != 'admin':
+        abort(403)
+
+
+def get_stock_name(stock_symbol):
+    url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=demo'
+    url_with_apikey = url.replace('demo', API_KEY)
+    url_with_symbol = url_with_apikey.replace('IBM', stock_symbol)
+    r = requests.get(url_with_symbol)
+    data = r.json()
+    return data.get('Name')
+
+
+@bp.route('/history')
+def history():
+    check_admin()
+    db = get_db()
+    portfolio_objects = db.execute("""
+    SELECT po.ticker, SUM(po.quantity) AS total_shares_held, t.unitPrice AS price_per_share
+    FROM PortfolioObjects po
+    JOIN Transactions t ON po.ticker = t.ticker
+    GROUP BY po.ticker
+    ORDER BY po.ticker;
+    """).fetchall()
+
+    history_data = []
+    for portfolio_object in portfolio_objects:
+        stock_name = get_stock_name(portfolio_object['ticker'])
+        history_data.append({
+            'ticker': portfolio_object['ticker'],
+            'name': stock_name,
+            'total_shares_held': portfolio_object['total_shares_held'],
+            'price_per_share': portfolio_object['price_per_share']
+        })
+
+    return render_template('admin/history.html', history_data=history_data)
+
+
+@bp.route('/summary')
+def summary():
+    check_admin()
+    db = get_db()
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    transactions = db.execute("""
+    SELECT t.ticker,
+           SUM(CASE WHEN t.orderType = 'buy' THEN t.amount ELSE 0 END) AS shares_bought,
+           SUM(CASE WHEN t.orderType = 'sell' THEN t.amount ELSE 0 END) AS shares_sold
+    FROM Transactions t
+    WHERE DATE(t.dateTime) = ?
+    GROUP BY t.ticker
+    ORDER BY t.ticker;
+    """, (current_date,)).fetchall()
+
+    summary_data = []
+    for transaction in transactions:
+        stock_name = get_stock_name(transaction['ticker'])
+        summary_data.append({
+            'ticker': transaction['ticker'],
+            'name': stock_name,
+            'shares_bought': transaction['shares_bought'],
+            'shares_sold': transaction['shares_sold']
+        })
+
+    return render_template('admin/summary.html', summary_data=summary_data, current_date=current_date)
