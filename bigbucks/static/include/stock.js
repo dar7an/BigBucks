df8dea13d99b60276187b87a1d4a213d4cfadaa1
Shiv <shiv.patel@duke.edu>
2024-04-21 16:24:52 -0400

    Fix: How stock search tool plots are created

--- bigbucks/static/include/stock.js
+++ bigbucks/static/include/stock.js
@@ -1,16 +1,23 @@
 let apiKey
 
-fetch('/config')
-    .then(response => response.json())
-    .then(config => {
-        apiKey = config.API_KEY;
-    });
+function fetchConfigAndMakePlotAPI(stockSymbol) {
+    fetch('/config')
+        .then(response => response.json())
+        .then(config => {
+            apiKey = config.API_KEY;
+            makePlotAPI(stockSymbol);
+        });
+}
 
-function makePlotAPI(stockSymbol) {;
+function makePlotAPI(stockSymbol) {
     fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockSymbol}&outputsize=full&apikey=${apiKey}`)
         .then(response => response.json())
         .then(data => {
-            console.log("using API")
+            if (!data['Time Series (Daily)']) {
+                console.error('Time Series (Daily) data is not available');
+                return;
+            }
+            console.log("Creating Plot")
             const dates = Object.keys(data['Time Series (Daily)']).reverse();
             const adjClosePrices = dates.map(date => parseFloat(data['Time Series (Daily)'][date]['4. close']));
 
@@ -41,39 +48,3 @@ function makePlotAPI(stockSymbol) {;
         });
 }
 
-function makePlotnonAPI(stockSymbol, stock_data_json) {
-    console.log("NOT using API");
-    const stock_data = JSON.parse(stock_data_json);
-    chartData = [];
-
-    if (typeof stock_data === 'object' && stock_data !== null) {
-        Object.keys(stock_data).forEach(stock_symbol => {
-            const dates = Object.keys(stock_data[stock_symbol]);
-
-            const x = dates.map(date => date); // Extracting dates
-            const y = dates.map(date => parseFloat(stock_data[stock_symbol][date]["close_price"])); // Extracting close prices
-
-            chartData.push({
-                x: x,
-                y: y,
-                type: 'line',
-                line: {color: 'blue'}
-            });
-        });
-    } else {
-        console.error('Invalid stock_data format. Expected an object.', stock_data);
-    }
-
-    const layout = {
-        title: `Stock Prices for ${stockSymbol}`,
-        xaxis: {
-            title: 'Trading Day'
-        },
-        yaxis: {
-            title: 'Close Price'
-        }
-    };
-
-    console.log('Creating Plotly chart');
-    Plotly.newPlot('myChart', chartData, layout);
-}
